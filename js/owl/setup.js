577259f9a02fc5a31233be0db889be6a706e1320
Kassia <kassia11919@gmail.com>
2022-02-02 17:51:55 -0300

    projeto finalizado

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -14,3 +14,16 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+var el = document.getElementsByClassName('botao-amazon1')[0];
+
+el.addEventListener("click", function(){
+    window.open("https://www.amazon.com.br/gp/bestsellers/books/?ie=UTF8&ref_=sv_b_2")
+});
+
+var el1 = document.getElementsByClassName('botao-amazon2')[0];
+
+el1.addEventListener("click", function(){
+    window.open("https://www.amazon.com.br/gp/most-wished-for/books/")
+});
+
